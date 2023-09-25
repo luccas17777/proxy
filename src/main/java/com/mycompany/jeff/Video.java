28a191c2f14dc1bd9aee1b49b6ff2a8eba0f5f46
luccas eduardo <luccasalzira@gmail.com>
2023-09-25 20:59:17 -0300

    c

---
+++ src/main/java/com/mycompany/jeff/Video.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.jeff;
+
+/**
+ *
+ * @author FATEC ZONA LESTE
+ */
+public class Video {
+    public String id;
+    public String title;
+    public String data;
+
+    Video(String id, String title) {
+        this.id = id;
+        this.title = title;
+        this.data = "Random video.";
+    }
+    
+}
